f54154aced09b134c13a7efe1472c73303c1c9d0
Forrest Dunlap <fdunlap@mighty.mangobutt.com>
2024-01-02 15:24:54 -0500

    Fix page data types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,15 +1,22 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { SupabaseClient } from '@supabase/supabase-js';
+import type { Session } from '@supabase/supabase-js';
+import type { Database } from './types/supabaseDB';
+
+type TypedSupabase = SupabaseClient<Database>;
+
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			supabase: import('@supabase/supabase-js').SupabaseClient<
-				import('./types/supabaseDB').Database
-			>;
-			getSession: () => Promise<import('@supabase/supabase-js').Session | null>;
+			supabase: TypedSupabase;
+			getSession: () => Promise<Session | null>;
 		}
-		// interface PageData {}
+		interface PageData {
+			session: Session | null;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
     export let data;
+
+    console.log(JSON.stringify(data, null, 2));
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-
-<textarea>{JSON.stringify(data, null, 2)}</textarea>
