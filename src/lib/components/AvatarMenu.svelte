5b256a88d487eaded2c8ec7e1bdb33dde5c07d86
Forrest Dunlap <forrestmdunlap@gmail.com>
2024-01-02 23:40:23 -0500

    Add popover

--- src/lib/components/AvatarMenu.svelte
+++ src/lib/components/AvatarMenu.svelte
@@ -1,11 +1,32 @@
 <script lang="ts">
 	import type { Session } from "@supabase/supabase-js";
+    import * as Popover from "$lib/components/ui/popover";
 
     export let session: Session;
 </script>
 
-<div class="h-8 w-8 rounded-full">
-    <img src={session.user?.user_metadata.avatar_url} alt="User avatar" class="h-full rounded-full w-full"/>
+<div class="h-10 w-10 rounded-full">
+    <Popover.Root>
+        <Popover.Trigger>
+            <img src={session.user?.user_metadata.avatar_url} alt="User avatar" class="h-full rounded-full w-full"/>
+        </Popover.Trigger>
+        <Popover.Content>
+            <div class="flex flex-col gap-y-2">
+                <div class="flex flex-row gap-x-2">
+                    <span class="font-bold">Email:</span>
+                    <span>{session.user?.email}</span>
+                </div>
+                <div class="flex flex-row gap-x-2">
+                    <span class="font-bold">Name:</span>
+                    <span>{session.user?.user_metadata.full_name}</span>
+                </div>
+                <div class="flex flex-row gap-x-2">
+                    <span class="font-bold">ID:</span>
+                    <span>{session.user?.id}</span>
+                </div>
+            </div>
+        </Popover.Content>
+    </Popover.Root>
 </div>
 
 <style>
