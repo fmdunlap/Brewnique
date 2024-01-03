f354e47d7eef17bc4168ddb387b81e4b9669f3f4
Forrest Dunlap <fdunlap@mighty.mangobutt.com>
2024-01-03 18:54:11 -0500

    Add shallow routing dialog in HeaderBar

--- src/lib/components/HeaderBar.svelte
+++ src/lib/components/HeaderBar.svelte
@@ -1,9 +1,39 @@
 <script lang="ts">
 	import type { Session, SupabaseClient } from '@supabase/supabase-js';
 	import AvatarMenu from './AvatarMenu.svelte';
+	import { goto, preloadData, pushState } from '$app/navigation';
+	import LoginPage from '../../routes/login/+page.svelte';
+	import { page } from '$app/stores';
+	import * as Dialog from '$lib/components/ui/dialog';
+	import { superValidate } from 'sveltekit-superforms/server';
+	import { LoginFormSchema } from '$lib/types/forms';
+	import type { Database } from '$lib/types/supabaseDB';
 
-	export let supabase: SupabaseClient;
+	export let supabase: SupabaseClient<Database>;
 	export let session: Session | null;
+
+	$: loginState = {
+		supabase: supabase,
+		session: session,
+		form: null
+	};
+
+	$: loginDialogOpen = loginState.form != null;
+
+	async function onLoginPressed(e: MouseEvent & { currentTarget: HTMLAnchorElement }) {
+		if (e.metaKey || e.ctrlKey) return;
+		e.preventDefault();
+
+		const { href } = e.currentTarget;
+
+		const result = await preloadData(href);
+		if (result.type === 'loaded' && result.status === 200) {
+			loginState.form = result.data.form;
+			pushState('', { loginOpen: true });
+		} else {
+			goto(href);
+		}
+	}
 </script>
 
 <div class="sticky top-0 flex min-w-full flex-row justify-between bg-orange-500 px-4 py-4">
@@ -15,7 +45,21 @@
 		{#if session != null}
 			<AvatarMenu {supabase} {session} />
 		{:else}
-			<!-- <SignInDialog {supabase} /> -->
+			<a href="/login" on:click={onLoginPressed}>Log In</a>
 		{/if}
 	</div>
 </div>
+
+<Dialog.Root
+	open={loginDialogOpen}
+	onOpenChange={(open) => {
+		if (!open) {
+			loginState.form = null;
+			history.back();
+		}
+	}}
+>
+	<Dialog.Content class="md:w-2/3 md:max-w-full md:p-0">
+		<LoginPage data={loginState} />
+	</Dialog.Content>
+</Dialog.Root>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,25 +3,36 @@
 
 	import SignInForm from '$lib/components/auth/SignInForm.svelte';
 	import SsoButtons from '$lib/components/auth/SSOButtons.svelte';
+	import type { Session, SupabaseClient } from '@supabase/supabase-js';
+	import type { Database } from '$lib/types/supabaseDB';
+	import type { SuperValidated } from 'sveltekit-superforms';
+	import type { LoginFormSchema } from '$lib/types/forms';
 
-	export let data;
+	export let data: {
+		session: Session | null;
+		supabase: SupabaseClient<Database>;
+		form: SuperValidated<typeof LoginFormSchema> | null;
+	};
 </script>
 
 <div
 	class="my-auto flex flex-col rounded-xl border-2 border-orange-900 bg-orange-700 shadow-xl md:flex-row"
 >
-	<div class="im-div m-auto flex w-screen flex-grow rounded-l-xl" />
-	<div class="m-auto flex w-2/3 flex-col gap-y-4 px-8 py-8">
+	<div
+		class="im-div m-auto flex w-full flex-grow rounded-t-xl md:rounded-l-xl md:rounded-tr-none"
+	/>
+	<div class="m-auto flex w-2/3 flex-col gap-y-4 py-8 md:px-8">
 		<SsoButtons supabase={data.supabase} />
 		<Separator />
-		<SignInForm data={data.form} />
+		{#if data.form}
+			<SignInForm data={data.form} />
+		{/if}
 	</div>
 </div>
 
 <style>
 	.im-div {
 		height: calc(50vh);
-		width: 90%;
 		background-image: url('$lib/assets/images/signinup-background.png');
 		background-size: cover;
 		background-position: center;
