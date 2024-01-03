0d5b97ab58947ce9241cf1f0e72a7689252d4811
Forrest Dunlap <forrestmdunlap@gmail.com>
2024-01-02 23:47:37 -0500

    Migrate popover -> dropdown

--- src/lib/components/AvatarMenu.svelte
+++ src/lib/components/AvatarMenu.svelte
@@ -1,32 +1,24 @@
 <script lang="ts">
 	import type { Session } from "@supabase/supabase-js";
-    import * as Popover from "$lib/components/ui/popover";
+    import * as Dropdown from "$lib/components/ui/dropdown-menu";
 
     export let session: Session;
 </script>
 
 <div class="h-10 w-10 rounded-full">
-    <Popover.Root>
-        <Popover.Trigger>
+    <Dropdown.Root>
+        <Dropdown.Trigger>
             <img src={session.user?.user_metadata.avatar_url} alt="User avatar" class="h-full rounded-full w-full"/>
-        </Popover.Trigger>
-        <Popover.Content>
-            <div class="flex flex-col gap-y-2">
-                <div class="flex flex-row gap-x-2">
-                    <span class="font-bold">Email:</span>
-                    <span>{session.user?.email}</span>
-                </div>
-                <div class="flex flex-row gap-x-2">
-                    <span class="font-bold">Name:</span>
-                    <span>{session.user?.user_metadata.full_name}</span>
-                </div>
-                <div class="flex flex-row gap-x-2">
-                    <span class="font-bold">ID:</span>
-                    <span>{session.user?.id}</span>
-                </div>
-            </div>
-        </Popover.Content>
-    </Popover.Root>
+        </Dropdown.Trigger>
+        <Dropdown.Content class="w-72">
+            <Dropdown.Label>Profile</Dropdown.Label>
+            <Dropdown.Group>
+                <Dropdown.Item>Some Item</Dropdown.Item>
+                <Dropdown.Item>Another Item</Dropdown.Item>
+                <Dropdown.Item>Yet More Thing</Dropdown.Item>
+            </Dropdown.Group>
+        </Dropdown.Content>
+    </Dropdown.Root>
 </div>
 
 <style>
