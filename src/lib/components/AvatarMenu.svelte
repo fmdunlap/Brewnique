643710c84170922c986d8fca2b673c8c5d542ad8
Forrest Dunlap <forrestmdunlap@gmail.com>
2024-01-03 19:22:11 -0500

    Formatting Fixes (#2)
    * Update formating
    
    * Create node.js.yml action
    
    * Update and rename node.js.yml to bun.js.yml
    
    * Update bun.js.yml
    
    * Update bun.js.yml
    
    * Lint bun.js.yml
    
    * Add shadcn and .github file to eslintingore
    
    * Add .github file to prettierignore
    
    ---------
    
    Co-authored-by: Forrest Dunlap <fdunlap@mighty.mangobutt.com>

--- src/lib/components/AvatarMenu.svelte
+++ src/lib/components/AvatarMenu.svelte
@@ -1,34 +1,34 @@
 <script lang="ts">
-	import type { Session, SupabaseClient } from "@supabase/supabase-js";
-    import * as Dropdown from "$lib/components/ui/dropdown-menu";
-	import * as Avatar from "$lib/components/ui/avatar";
-	import { invalidateAll } from "$app/navigation";
+	import type { Session, SupabaseClient } from '@supabase/supabase-js';
+	import * as Dropdown from '$lib/components/ui/dropdown-menu';
+	import * as Avatar from '$lib/components/ui/avatar';
+	import { invalidateAll } from '$app/navigation';
 
-    export let session: Session;
-    export let supabase: SupabaseClient;
+	export let session: Session;
+	export let supabase: SupabaseClient;
 
-    const signOut = async () => {
-        await supabase.auth.signOut();
-        invalidateAll();
-    }
+	const signOut = async () => {
+		await supabase.auth.signOut();
+		invalidateAll();
+	};
 </script>
 
 <div class="h-10 w-10 rounded-full">
-    <Dropdown.Root>
-        <Dropdown.Trigger>
-            <Avatar.Root>
-                <Avatar.Image src={session.user?.user_metadata.avatar_url} alt="Avatar" />
-                <Avatar.Fallback>{session.user?.email?.slice(0,1)}</Avatar.Fallback>
-            </Avatar.Root>
-        </Dropdown.Trigger>
-        <Dropdown.Content class="w-72">
-            <Dropdown.Label>Profile</Dropdown.Label>
-            <Dropdown.Group>
-                <Dropdown.Item>Some Item</Dropdown.Item>
-                <Dropdown.Item>Another Item</Dropdown.Item>
-                <Dropdown.Item>Yet More Thing</Dropdown.Item>
-                <Dropdown.Item on:click={signOut}>Log Out</Dropdown.Item>
-            </Dropdown.Group>
-        </Dropdown.Content>
-    </Dropdown.Root>
+	<Dropdown.Root>
+		<Dropdown.Trigger>
+			<Avatar.Root>
+				<Avatar.Image src={session.user?.user_metadata.avatar_url} alt="Avatar" />
+				<Avatar.Fallback>{session.user?.email?.slice(0, 1)}</Avatar.Fallback>
+			</Avatar.Root>
+		</Dropdown.Trigger>
+		<Dropdown.Content class="w-72">
+			<Dropdown.Label>Profile</Dropdown.Label>
+			<Dropdown.Group>
+				<Dropdown.Item>Some Item</Dropdown.Item>
+				<Dropdown.Item>Another Item</Dropdown.Item>
+				<Dropdown.Item>Yet More Thing</Dropdown.Item>
+				<Dropdown.Item on:click={signOut}>Log Out</Dropdown.Item>
+			</Dropdown.Group>
+		</Dropdown.Content>
+	</Dropdown.Root>
 </div>
--- src/routes/dev/+page.svelte
+++ src/routes/dev/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    let links = [
-        { name: "Components", href: "/dev/components" },
-        { name: "Layout", href: "/dev/layout" },
-        { name: "Page", href: "/dev/page" },
-        { name: "Supabase", href: "/dev/supabase"}
-    ]
+	let links = [
+		{ name: 'Components', href: '/dev/components' },
+		{ name: 'Layout', href: '/dev/layout' },
+		{ name: 'Page', href: '/dev/page' },
+		{ name: 'Supabase', href: '/dev/supabase' }
+	];
 </script>
 
 <div>
-    <ul>
-        {#each links as link}
-            <li><a href={link.href}>{link.name}</a></li>
-        {/each}
-    </ul>
+	<ul>
+		{#each links as link}
+			<li><a href={link.href}>{link.name}</a></li>
+		{/each}
+	</ul>
 </div>
