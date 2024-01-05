bc1fc67a134eeb541cb86de36c76ef6db1fdb0a4
Forrest Dunlap <forrestmdunlap@gmail.com>
2024-01-05 18:38:59 -0500

    Simplify SSO Buttons

--- src/lib/components/auth/ContinueWithEmail.svelte
+++ src/lib/components/auth/ContinueWithEmail.svelte
@@ -5,7 +5,7 @@
 
 <Button variant="secondary" on:click>
 	<div class="my-auto flex w-full flex-row">
-		<Mail />
+		<Mail class="mr-[12px] h-[20px] w-[20px] min-w-[20px]" />
 		<span class="mx-auto">Continue with Email</span>
 		<span class="sr-only">Continue with Email</span>
 	</div>
