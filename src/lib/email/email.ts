ffec18b91548a003c90c57b02048fcc533dfa400
Forrest Dunlap <forrestmdunlap@gmail.com>
2024-01-24 15:34:43 -0600

    Add nullcheck to Postmark API key

--- src/lib/email/email.ts
+++ src/lib/email/email.ts
@@ -1,6 +1,10 @@
 import { env } from '$env/dynamic/private';
 import postmark from 'postmark';
 
+if (!env.POSTMARK_API_KEY) {
+	throw new Error('POSTMARK_API_KEY is not set');
+}
+
 const client = new postmark.ServerClient(env.POSTMARK_API_KEY);
 
 export async function sendEmail(to: string, subject: string, html: string, text?: string) {
