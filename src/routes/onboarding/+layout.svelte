e82104127e577f0c59c131dc6826ef985c715628
Forrest Dunlap <forrestmdunlap@gmail.com>
2024-02-04 10:10:31 -0600

    Prettier formatting

--- src/routes/onboarding/+layout.svelte
+++ src/routes/onboarding/+layout.svelte
@@ -1,5 +1,5 @@
 <p class="mx-auto w-full py-10 text-center text-4xl">Welcome!</p>
 
-<div class="flex flex-col rounded border-2 border-secondary p-4 shadow-xl">
+<div class="border-secondary flex flex-col rounded border-2 p-4 shadow-xl">
 	<slot />
 </div>
