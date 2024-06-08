44eaa432063a1f953cd50332e1ab4e9070fe5f16
Forrest Dunlap <fdunlap@mighty.mangobutt.com>
2024-06-08 17:52:53 -0500

    Fix migration up

--- migrations/000002_add_recipe_check_constraints.up.sql
+++ migrations/000002_add_recipe_check_constraints.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE recipes ADD CONSTRAINT recipe_name_check CHECK (name IS NOT NULL AND name != '');
-ALTER TABLE recipes ADD CONSTRAINT recipe_ingredients_check_constraint CHECK (array_length(ingredients, 1) > 0 AND ingredients IS NOT NULL AND ingredients != '');
-ALTER TABLE recipes ADD CONSTRAINT recipe_instructions_check_constraint CHECK (array_length(instructions, 1) > 0 AND instructions IS NOT NULL AND instructions != '');
+ALTER TABLE recipes ADD CONSTRAINT recipe_ingredients_check_constraint CHECK (array_length(ingredients, 1) > 0 AND ingredients IS NOT NULL);
+ALTER TABLE recipes ADD CONSTRAINT recipe_instructions_check_constraint CHECK (array_length(instructions, 1) > 0 AND instructions IS NOT NULL);
