1be843443f0b0caa5d3a64afc063339f9e1a9378
Forrest Dunlap <fdunlap@mighty.mangobutt.com>
2024-06-08 12:26:27 -0500

    Connect frontend to backend + add cors middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -3,11 +3,26 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
-func (app *application) routes() chi.Router {
-	r := chi.NewRouter()
+func applyMiddleware(r chi.Router) chi.Router {
 	r.Use(middleware.Logger)
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	return r
+}
+
+func (app *application) routes() chi.Router {
+	r := applyMiddleware(chi.NewRouter())
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/healthcheck", app.healthcheckHandler)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,45 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [id, setId] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [recipe, setRecipe] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      console.log(e.target.value);
+      if (e.target.value === "") {
+          return;
+      }
+      setId(parseInt(e.target.value));
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setLoading(true);
+      const response = await fetch(`http://localhost:8080/v1/recipe/${id}`);
+      const data = await response.json();
+      console.log(data);
+      setLoading(false);
+      setId(0);
+      setRecipe(JSON.stringify(data, null, 2));
+  }
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="bg-red-500">
-        <button
-          className="mx-auto"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
+        <h1>Brewnique</h1>
+        <form onSubmit={handleSubmit}>
+            <div className="flex flex-col">
+                <label htmlFor="id">recipe id</label>
+                <input type="number" id="id" name="id" value={id} onChange={handleChange} />
+            </div>
+            <button type="submit">
+                {loading ? <span>Loading...</span> : <span>Submit</span>}
+            </button>
+        </form>
+        <div className="flex flex-col">
+            <label htmlFor="recipe">recipe</label>
+            {recipe && <pre>{recipe}</pre>}
+        </div>
     </>
   );
 }
